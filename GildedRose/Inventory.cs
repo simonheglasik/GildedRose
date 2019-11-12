f87abd773fb2621af317f55b49ec0e07c9f258f6
simonheglasik <simonheglasik@gmail.com>
2019-11-12 16:05:48 +0100

    GildedRose

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-
 namespace GildedRose
 {
     class Inventory
@@ -24,6 +23,52 @@ namespace GildedRose
         {
             // TODO ...
             // Hint: Iterate through this.items and check Name property to access specific item
+            foreach (var item in items)
+            {
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    item.SellIn--;
+                    if (item.Quality >= 0 & item.Quality <= 50)
+                    {
+                        if (item.Name == "Aged Brie")
+                        {
+                            item.Quality++;
+                            if (item.SellIn < 0)
+                                item.Quality++;
+                        }
+                        else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            item.Quality++;
+                            if (item.SellIn < 10)
+                                item.Quality ++;
+                            if (item.SellIn < 5)
+                                item.Quality++;
+                            if (item.SellIn <= 0)
+                                item.Quality = 0;
+                        }
+                        else if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            item.Quality -= 1;
+                            if (item.Name.Contains("Conjured"))
+                            {
+                                item.Quality--;
+                                if (item.SellIn <= 0)
+                                {
+                                    item.Quality--;
+                                }
+                            }
+                            if (item.SellIn <= 0)
+                            {
+                                item.Quality--;
+                            }
+                        }
+                    }
+                    if (item.Quality < 0)
+                        item.Quality = 0;
+                    if (item.Quality > 50)
+                        item.Quality = 50;
+                }
+            }
         }
     }
 }
